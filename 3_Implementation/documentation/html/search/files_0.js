70506116a13e651343e9b9a254754e7f7f0acfec
MohanBabuS <74193913+MohanBabuS@users.noreply.github.com>
2022-03-30 22:17:45 +0530

    update documentation

--- 3_Implementation/documentation/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['quiz_5foperations_2eh_0',['quiz_operations.h',['../quiz__operations_8h.html',1,'']]]
-];
--- 3_Implementation/documentation/html/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['reset_5fscore_0',['reset_score',['../quiz__operations_8h.html#aaf42193539144c0966542d7da8965c7b',1,'quiz_operations.h']]]
-];
